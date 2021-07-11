9228e80fe31c698760519fa24b7d47f483c5a0d9
melithe360 <melithe360@naver.com>
2021-07-11 13:58:12 +0900

    21.07.11 heein update : login, signup, find id/pw page

--- seed/src/App.js
+++ seed/src/App.js
@@ -1,16 +1,44 @@
 import './App.css';
 import React, { Component } from 'react';
-import Navi from './Components/Navi'
-import './Components/Navi.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navi from './Components/Navi';
+import './Components/Navi.css';
+import Home from './Components/Home/Home.js';
+import Login from './Components/Login/Login.js';
+import Signup from './Components/Signup/Signup.js';
+import SeeMain from './Components/See/SeeMain.js';
+import LikeMain from './Components/Like/LikeMain.js';
+import MyPageMain from './Components/MyPage/MyPage.js';
+import Find from './Components/Find/Find.js';
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <Navi></Navi>
-      </div>
-    );
-  }
+
+const App = () =>{
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/see" component={SeeMain} />
+          <Route path="/like" component={LikeMain} />
+          <Route path="/mypage" component={MyPageMain} />
+          <Route path="/find" component={Find} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+
+  )
 }
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <div className="App">
+//         <Navi></Navi>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- seed/src/Components/Navi.js
+++ seed/src/Components/Navi.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Navi.css'
 import toc from './Logo/toc-white-18dp.svg'
+
 class Navi extends Component {
     render(){
        return (
             <header>
                 <div class="menu_wrapper">
                     <button type="button" class="btn_toc">
-                    //<img id= "toc_img" src={toc} width="30" height="30"/>
+                    <img id= "toc_img" src={toc} width="30" height="30"/>
                     </button>
-                    <h1 class="logo">Seed Logo</h1>
+                    <Link to="/"><h1 class="logo">seed</h1></Link>
                     <nav>
                         <ul class="menu">
-                            <li><a href="/See/see_main.js">See</a></li>
-                            <li><a href="/Like/like_main.js">Like</a></li>
-                            <li><a href="/Mypage/mypage.js ">MyPage</a></li>
+                            <Link to="/see"><li>SEE</li></Link>
+                            <Link to="/like"><li>LIKE</li></Link>
+                            <Link to="/mypage"><li>MY PAGE</li></Link>
                         </ul>
+
+                        <Link to="/login"><button class="btn" id="loginBtn">Login</button></Link>
+                        <Link to="/signup"><button class="btn" id="signupBtn">Sign up</button></Link>
                     </nav>
-                    <div class = "user">
-                        <p id = "user_name">Seed님</p>
+                    <div class="user">
+                        {/* <p id = "user_name">Seed님</p> */}
                         {/* <img id = "user_img" src={require("frog-5319326_1920.jpg")} width="40" height="40"/> */}
                     </div>
                 </div>
@@ -27,4 +32,4 @@ class Navi extends Component {
      }
    }
 
-   export default Navi
+   export default Navi;
